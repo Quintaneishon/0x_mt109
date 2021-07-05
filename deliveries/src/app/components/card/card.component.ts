dff28b4146ad16f2f43c305854ef5d55070c6972
Ajitzi <ajitzi.quintana@gmail.com>
2021-07-04 21:39:21 -0500

    Finish frontend design

---
+++ deliveries/src/app/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CardModel } from 'src/app/models/card.model';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() card: CardModel;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ deliveries/src/app/models/card.model.ts
@@ -0,0 +1,9 @@
+export interface CardModel{
+    ruta: string,
+    conductor: string,
+    creada: string,
+    inicio: string,
+    termino: string,
+    entregas: number,
+    completada: boolean 
+}
